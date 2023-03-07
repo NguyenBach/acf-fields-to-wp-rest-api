9807389c03b344ad5338b2d13420baa0d9a11d78
Bach Nguyen <NguyenBach@users.noreply.github.com>
2023-03-07 10:02:16 +0700

    Add woocomerce filter

--- taxonomies.php
+++ taxonomies.php
@@ -6,6 +6,33 @@ function acf2api_hook_all_terms(){
   $taxonomies = array_keys( get_taxonomies() );
 
   foreach ($taxonomies as $taxonomy) {
+    
+    add_filter('woocommerce_rest_prepare_'.$taxonomy, function($data, $term, $request){
+
+      //Get the response data
+      $response_data = $data->get_data();
+
+      //Bail early if there's an error
+      if ( $request['context'] !== 'view' || is_wp_error( $data ) ) {
+          return $data;
+      }
+
+      //Get all fields
+      $fields = get_fields('term_'.$term->term_id);
+      //If we have fields...
+      if ($fields){
+        //Loop through them...
+        foreach ($fields as $field_name => $value){
+          //Set the meta
+          $response_data[$field_name] = $value;
+        }
+      }
+
+      //Commit the API result var to the API endpoint
+      $data->set_data( $response_data );
+      return $data;
+
+    }, 10, 3);
 
     add_filter( 'rest_prepare_'.$taxonomy, function($data, $term, $request){
 
